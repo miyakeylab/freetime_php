1dc9e8505260ebbf7864eb881c8b9857d727cc97
miyakey <miyasaviola@gmail.com>
2017-08-26 16:49:54 +0000

    sheed追加

--- laravel/database/migrations/2017_08_17_084825_create_offers_table.php
+++ laravel/database/migrations/2017_08_17_084825_create_offers_table.php
@@ -18,7 +18,13 @@ class CreateOffersTable extends Migration
             $table->unsignedInteger('master_user_id');//オファーユーザーID
             $table->unsignedInteger('client_user_id');//オファークライアントユーザーID
             $table->unsignedInteger('state');         //オファー状況
+            $table->unsignedInteger('my_time_id');  //時間ID
+            $table->datetime('start_time');         //スケジュール開始時間
+            $table->datetime('end_time');           //スケジュール終了時間
+            $table->datetime('start_time_gmt');     //スケジュール開始時間(GMT)
+            $table->datetime('end_time_gmt');       //スケジュール終了時間(GMT)
             $table->string('content');                //内容
+            $table->unsignedInteger('category_id'); //カテゴリーID
             $table->timestamps();
         });
     }
--- laravel/resources/views/group.blade.php
+++ laravel/resources/views/group.blade.php
@@ -9,6 +9,34 @@
         <button type="submit" class="btn btn-success">
         <i class="fa fa-btn fa-users"></i> グループ新規作成</button>
         <div class="padding-top-10">
+        
+        @if (count($groups) > 0)
+        <div class="panel panel-default">
+        <div class="panel-heading">グループ一覧</div>
+        <div class="panel-body">
+        <!-- 友達一覧 -->
+            @foreach ($groups as $group)
+            <div class="media">
+                <!-- 1.画像の配置 -->
+                <a class="media-left" href="#">
+                   <img class="media-object" src="{{url($group->group_img)}}">
+                </a>
+                <!-- 2.画像の説明 -->
+                <div class="media-body">
+                    @if($group->group_name !== "")
+                    <h4 class="media-heading">{{ $group->group_name }}</h4>
+                    @endif
+                </div>
+                <div class="media-right">
+                    <button type="submit" class="btn btn-danger">
+                    <i class="fa fa-btn fa-frown-o"></i> 退会</button>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+            </div>
+            </div>
+        @endif
         @if (count($groups) > 0)
         <div class="panel panel-default">
         <div class="panel-heading">グループ申請</div>
