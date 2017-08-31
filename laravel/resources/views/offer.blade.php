cdb5b48d251d068434134d4c76c99e91707fed58
miyakey <miyasaviola@gmail.com>
2017-08-31 03:17:54 +0000

    オファー修正

--- laravel/resources/views/offer.blade.php
+++ laravel/resources/views/offer.blade.php
@@ -7,20 +7,43 @@
 @section('content')
     <!-- オファー -->
     <div class="container" >
+        <div class="col-md-8 col-md-offset-2"> 
         <button type="submit" class="btn btn-success" >
         <i class="fa fa-btn fa-envelope-o"></i> オファー作成</button>
-        <div class="padding-top-10">
-        <div class="table-responsive">
+        <div class="padding-top-10"></div>
+        </div>
         @if (count($offers) > 0)
-        <!-- オファー一覧 -->
-        <table class="table table-bordered table-condensed" style="font-size : 5px;">
+        <div class="row">
+        <div class="col-md-8 col-md-offset-2">  
+        <div class="panel panel-default">
+        <div class="panel-heading">受領オファー一覧</div>
+        <div class="panel-body">
+            <!-- 受領オファー一覧 -->
+            <?php $i = 0; ?>
             @foreach ($offers as $offer)
-            <tr>
-                <td colspan="2" align="center">{{ Config::get('const.OFFER_STRING')[$offer->state] }}</td>
-                <td colspan="2" align="center">{{ $offer->content }}</td>
-            </tr>
+            <?php if($i !== 0){ ?>
+            <hr class="style-one">
+            <?php } ?>
+            <?php $i++; ?>
+            <div class="media">
+                <!-- 1.画像の配置 -->
+                <a class="media-left" href="#">
+                   {{ Config::get('const.OFFER_STRING')[$offer->state] }}
+                </a>
+                <!-- 2.画像の説明 -->
+                <div class="media-body">
+                    <h4 class="media-heading">{{ $offer->content }}</h4>
+                </div>
+                <div class="media-right">
+                    <button type="submit" class="btn btn-danger">
+                    <i class="fa fa-btn fa-frown-o"></i> 拒否</button>
+                </div>
+            </div>
             @endforeach
-        </table>
+            </div>
+            </div>           
+            </div>
+        </div>
         @endif
         </div>
     </div>
